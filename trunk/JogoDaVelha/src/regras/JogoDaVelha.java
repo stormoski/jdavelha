897c4c7be4e12ca8d40686f2cb809c638c6b93a6
linkpg <linkpg@248899c8-e7d9-11dd-bce9-bf72c0239d3a>
2009-04-14 03:44:36 +0000

    Import

--- trunk/JogoDaVelha/src/regras/JogoDaVelha.java
+++ trunk/JogoDaVelha/src/regras/JogoDaVelha.java
@@ -1,9 +1,15 @@
 package regras;
 
+import regras.eventos.OuvinteStatus;
+
 public interface JogoDaVelha {
-    void iniciar();
+    void iniciar(String jogador1, String jogador2);
     
     void jogar(int posicao);
     
     void atualizarStatus();
+
+    void addOuvinteStatus(OuvinteStatus ouvinte);
+
+    void removeOuvinteStatus(OuvinteStatus ouvinte);
 }
